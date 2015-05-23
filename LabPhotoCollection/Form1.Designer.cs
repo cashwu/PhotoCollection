d4fa926dc3c010da2e0c91558d876fb4f11b0661
cash <cash@cashwu.com>
2015-05-24 00:27:47 +0800

    Move Photo

--- LabPhotoCollection/Form1.Designer.cs
+++ LabPhotoCollection/Form1.Designer.cs
@@ -31,8 +31,17 @@
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.btnSelectPath = new System.Windows.Forms.Button();
             this.boxPhoto = new System.Windows.Forms.ListBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.boxErrorFile = new System.Windows.Forms.ListBox();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
             // 
             // btnSelectPath
             // 
@@ -49,37 +58,80 @@
             this.boxPhoto.FormattingEnabled = true;
             this.boxPhoto.HorizontalScrollbar = true;
             this.boxPhoto.ItemHeight = 20;
-            this.boxPhoto.Location = new System.Drawing.Point(22, 87);
+            this.boxPhoto.Location = new System.Drawing.Point(22, 127);
             this.boxPhoto.Name = "boxPhoto";
             this.boxPhoto.ScrollAlwaysVisible = true;
-            this.boxPhoto.SelectionMode = System.Windows.Forms.SelectionMode.None;
-            this.boxPhoto.Size = new System.Drawing.Size(243, 324);
+            this.boxPhoto.Size = new System.Drawing.Size(243, 284);
             this.boxPhoto.TabIndex = 1;
+            this.boxPhoto.SelectedIndexChanged += new System.EventHandler(this.boxPhoto_SelectedIndexChanged);
             // 
-            // listBox1
+            // boxErrorFile
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(291, 87);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(240, 324);
-            this.listBox1.TabIndex = 2;
+            this.boxErrorFile.FormattingEnabled = true;
+            this.boxErrorFile.HorizontalScrollbar = true;
+            this.boxErrorFile.ItemHeight = 20;
+            this.boxErrorFile.Location = new System.Drawing.Point(291, 127);
+            this.boxErrorFile.Name = "boxErrorFile";
+            this.boxErrorFile.ScrollAlwaysVisible = true;
+            this.boxErrorFile.Size = new System.Drawing.Size(240, 284);
+            this.boxErrorFile.TabIndex = 2;
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(553, 87);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(302, 214);
+            this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBox.TabIndex = 3;
+            this.imgBox.TabStop = false;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(156, 12);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(125, 53);
+            this.btnMove.TabIndex = 4;
+            this.btnMove.Text = "Start Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Error File List";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 434);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(882, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.boxErrorFile);
             this.Controls.Add(this.boxPhoto);
             this.Controls.Add(this.btnSelectPath);
             this.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Photo Collection";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,7 +140,11 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button btnSelectPath;
         private System.Windows.Forms.ListBox boxPhoto;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox boxErrorFile;
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- LabPhotoCollection/Form1.cs
+++ LabPhotoCollection/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,12 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExifLib;
 
 namespace LabPhotoCollection
 {
     public partial class Form1 : Form
     {
         private readonly string[] fileExtensions = new[] { "jpg" };
+        private string filePath;
+        private List<string> fullFileNames;
+        private Dictionary<string, string> dicExifDatas;
 
         public Form1()
         {
@@ -23,42 +28,148 @@ namespace LabPhotoCollection
         private void btnSelectPath_Click(object sender, EventArgs e)
         {
             folderBrowserDialog1.ShowDialog();
-            var selectPath = folderBrowserDialog1.SelectedPath;
-            if (string.IsNullOrWhiteSpace(selectPath))
+            filePath = folderBrowserDialog1.SelectedPath;
+
+            if (string.IsNullOrWhiteSpace(filePath))
             {
                 MessageBox.Show("請選擇資料夾");
+                return;
             }
 
-            var files = this.GetFiles(selectPath);
+            this.GetFullFileName();
+
+            if (this.fullFileNames.Count == 0)
+            {
+                MessageBox.Show("資料夾裡面沒有圖片檔");
+                return;
+            }
+
+            var fileNames = this.GetFileName(fullFileNames);
+
             boxPhoto.Items.Clear();
 
-            foreach (var file in files)
+            foreach (var fileName in fileNames)
             {
-                boxPhoto.Items.Add(file);
+                boxPhoto.Items.Add(fileName);
+            }
+
+            List<string> errorFileNames;
+            dicExifDatas = this.GetExifDateTime(fullFileNames, out errorFileNames);
+
+            boxErrorFile.Items.Clear();
+            foreach (var errorFileName in errorFileNames)
+            {
+                boxErrorFile.Items.Add(errorFileName);
             }
         }
 
-        private List<string> GetFiles(string path)
+        private void boxPhoto_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var files = new List<string>();
+            var box = (ListBox)sender;
+            var fileName = this.fullFileNames[box.SelectedIndex];
+            imgBox.ImageLocation = fileName;
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (this.dicExifDatas == null 
+                || this.dicExifDatas.Count == 0)
+            {
+                MessageBox.Show("請選擇資料夾");
+                return;
+            }
+
+            this.MovePic();
+        }
+
+        private void MovePic()
+        {
+            try
+            {
+                foreach (var dicExifData in this.dicExifDatas)
+                {
+                    var date = dicExifData.Value.Replace('/', '-');
+
+                    var newPath = this.filePath + "\\" + date;
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+
+                    var fileInfo = new FileInfo(dicExifData.Key);
+                    if (fileInfo.Exists)
+                    {
+                        var newFileInfo = new FileInfo(newPath + "\\" + fileInfo.Name);
+                        if (!newFileInfo.Exists)
+                        {
+                            fileInfo.MoveTo(newPath + "\\" + fileInfo.Name);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            MessageBox.Show("Done..");
+        }
+
+        private Dictionary<string, string> GetExifDateTime(List<string> fullFileNames, out List<string> errorFileNames)
+        {
+            var dic = new Dictionary<string, string>();
+            errorFileNames = new List<string>();
+
+            foreach (var fileName in fullFileNames)
+            {
+                try
+                {
+                    using (var reader = new ExifReader(fileName))
+                    {
+                        DateTime dt;
+                        reader.GetTagValue(ExifTags.DateTimeOriginal, out dt);
+
+                        if (!dic.ContainsKey(fileName))
+                        {
+                            dic.Add(fileName, dt.ToString("yyyy/MM/dd"));
+                        }
+                        else
+                        {
+                            errorFileNames.Add(fileName + " .. fileName 重複");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorFileNames.Add(fileName + " .. 無法解析");
+                }
+            }
+            
+            return dic;
+        }
+
+        private void GetFullFileName()
+        {
+            this.fullFileNames = new List<string>();
 
             foreach (var extension in fileExtensions)
             {
-                var tempFiles = Directory.GetFiles(path, "*." + extension);
+                var tempFilesNames = Directory.GetFiles(this.filePath, "*." + extension);
+                this.fullFileNames.AddRange(tempFilesNames);
+            }
+        }
 
-                if (tempFiles.Length == 0)
-                {
-                    continue;
-                }
+        private List<string> GetFileName(List<string> fullFileName)
+        {
+            var fileNames = new List<string>();
 
-                foreach (var tempFile in tempFiles)
-                {
-                    var fileInfo = new FileInfo(tempFile);
-                    files.Add(fileInfo.Name);
-                }
+            foreach (var tempFile in fullFileName)
+            {
+                var fileInfo = new FileInfo(tempFile);
+                fileNames.Add(fileInfo.Name);
             }
 
-            return files;
+            return fileNames;
         }
     }
 }
